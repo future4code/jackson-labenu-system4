25864a7352ad396ad749d1057afe252973fe30b9
RaphaelRPera <42681283+RaphaelRPera@users.noreply.github.com>
2020-11-14 00:36:49 -0300

    Update insertPerson.ts
    console.log's removidos

--- src/data/insertPerson.ts
+++ src/data/insertPerson.ts
@@ -2,11 +2,7 @@ import { connection } from "../index"
 
 export async function insertPerson(data: any): Promise<void>{
     let {person, name, email, birthdate} = data
-    console.log(`[insertPerson]: birthdate(1) =`, birthdate)
     const id = new Date().getTime()
-    console.log(`[insertPerson]: birthdate(2) =`, birthdate)
-
-    console.log(`[insertPerson]:`, person, id, name, email, birthdate)
 
     const sqlRaw = `
         INSERT INTO ${person.toUpperCase()} (id, name, email, birthdate)
@@ -19,5 +15,4 @@ export async function insertPerson(data: any): Promise<void>{
         ;
     `
     const result = await connection.raw(sqlRaw)
-    // console.log(result)
 }
